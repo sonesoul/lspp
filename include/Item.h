5acdb84796d19a076a9c0ba8cd913f470551fabf
sonesoul <smippy098@gmail.com>
2025-12-25 21:16:53 -0700

    split files to include and src

---
+++ include/Item.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <filesystem>
+
+struct Item {
+	std::filesystem::path path;
+};
---
+++ src/lspp.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argCount, char* argValues[]) {
+	setlocale(LC_ALL, "");
+	
+	return 0;
+}
