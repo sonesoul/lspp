d87c7fa03d2cadb21ab59df2c2752a6ef92e0c9c
sonesoul <smippy098@gmail.com>
2026-02-02 20:33:32 -0700

    move flags one bit back

--- include/lspp_flag.h
+++ include/lspp_flag.h
@@ -3,8 +3,8 @@ using u32 = unsigned int;
 
 enum lspp_flag : u32 {
 	None		   = 0,
-	Reversed	   = 1 << 1,
-	TypeReversed   = 1 << 2,
-	CaseSensitive  = 1 << 3,
-	RecursiveSize  = 1 << 4,
+	Reversed	   = 1 << 0,
+	TypeReversed   = 1 << 1,
+	CaseSensitive  = 1 << 2,
+	RecursiveSize  = 1 << 3,
 };
