fff82f558bf25608e5902e393dd0b76803de698c
sonesoul <smippy098@gmail.com>
2026-02-06 21:45:30 -0700

    remove SizeStrategy.h from size namespace

--- include/strategy/size/SizeStrategy.h
+++ include/strategy/size/SizeStrategy.h
@@ -2,9 +2,7 @@
 #include "Item.h"
 #include "strategy/Strategy.h"
 
-namespace size {
-	class SizeStrategy : Strategy {
-	public:
-		virtual uintmax_t operator()(Item&) = 0;
-	};
-}
+class SizeStrategy : Strategy {
+public:
+	virtual uintmax_t operator()(Item&) = 0;
+};
