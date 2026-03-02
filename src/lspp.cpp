821ba074e45ba66a436ffa0bcfa58ae25667d8a8
sonesoul <smippy098@gmail.com>
2026-03-02 12:11:18 -0700

    add preffered path handling; fix output locale

--- src/lspp.cpp
+++ src/lspp.cpp
@@ -10,46 +10,78 @@
 
 #include "fn/scan_directory.h"
 #include "fn/make_setter_map.h"
+#include <windows.h>
 
-using namespace std;
+namespace fs = std::filesystem;
 
 int main(int argc, char* argv[]) {
-	setlocale(LC_ALL, "");
-
-	auto setterMap = fn::make_setter_map();
-	auto config = lspp_config(
-		measure::files, 
-		priority::directories, 
-		compare::normal, 
-		predicate::by_name_ci);
 	
-	for (size_t i = 0; i < argc; ++i) {
-		auto param = argv[i];
-
-		if (setterMap.find(param) != setterMap.end()) {
-			setterMap[param](config);
-		}
-	}
-
-	auto base = std::filesystem::current_path();
-	
-	auto prio = config.get<Prioritizer>();
-	auto pred = config.get<Predicate>();
-	auto meas = config.get<Measurer>();
-	auto comp = config.get<Comparator>();
-
-	auto content = fn::scan_directory(base, meas);
-	std::sort(content.begin(), content.end(), [&](const auto& a, const auto& b) { return comp(a, b, prio, pred); });
-
-	for (size_t i = 0; i < content.size(); i++)
+	try
 	{
-		Item& item = content[i];
-		if (item.type() == ItemType::directory)
-			cout << "[] ";
-		else
-			cout << "-- ";
-		cout << item.name() << " : " << item.size() << "\n";
-	}
+		SetConsoleOutputCP(CP_UTF8);
+		SetConsoleCP(CP_UTF8);
 
+		auto base = fs::current_path();
+
+		auto setterMap = fn::make_setter_map();
+		auto config = lspp_config(
+			measure::files,
+			priority::directories,
+			compare::normal,
+			predicate::by_name_ci);
+
+		for (size_t i = 1; i < argc; ++i) {
+			auto param = argv[i];
+
+			auto it = setterMap.find(param);
+			if (it != setterMap.end()) {
+				it->second(config);
+			}
+			else if (fs::exists(param) &&
+				fs::is_directory(param)) {
+				base = fs::path(param);
+			}
+		}
+
+		std::cout << base.string() << "\n";
+
+		auto prio = config.get<Prioritizer>();
+		auto pred = config.get<Predicate>();
+		auto meas = config.get<Measurer>();
+		auto comp = config.get<Comparator>();
+
+		auto content = fn::scan_directory(base, meas);
+		std::cout << content.size() << "\n";
+
+		std::sort(content.begin(), content.end(), [&](const auto& a, const auto& b) { return comp(a, b, prio, pred); });
+
+
+		std::vector<std::string> sizeMeasures = { "B", "KB", "MB", "GB", "TB", "PB" };
+
+		for (size_t i = 0; i < content.size(); i++)
+		{
+			Item& item = content[i];
+			if (item.type() == ItemType::directory)
+				cout << "[] ";
+			else
+				cout << "-- ";
+
+			auto size = item.size();
+			size_t measureIndex = 0;
+
+			while (size >= 1024 && measureIndex < sizeMeasures.size()) {
+				size /= 1024;
+				++measureIndex;
+			}
+
+			cout << item.name() << " : " << size << sizeMeasures[measureIndex] << "\n";
+		}
+
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << ex.what();
+		return 0;
+	}
 	return 0;
 }
